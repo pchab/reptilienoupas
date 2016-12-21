658448de0ab2fc57a8ac8dacbe3db062e23060e6
pchab <pierre@chabardes.net>
2016-12-21 14:36:24 +0100

    add more famous reptilians

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
-            title: 'Reptilien ou pas ?',
+            title: 'Reptilian or not ?',
             message: err.message,
             error: err
         });
@@ -49,7 +49,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        title: 'Reptilien ou pas ?',
+        title: 'Reptilian or not ?',
         message: err.message,
         error: {}
     });
